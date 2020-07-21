f7c41cdf7dd8b77b116b9001a0c30e1bb1412db9
MarsDiplomatToEarth <60285644+MarsDiplomatToEarth@users.noreply.github.com>
2020-07-21 15:58:05 +0800

    Update jsonsql.py

--- jsonsql.py
+++ jsonsql.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import json,os,sys,codecs,chardet
 
-
 version = 2
 if sys.version_info >= (3,3):
     version = 3
@@ -13,67 +12,78 @@ else:
     pass
    
 #json file and dictionary engine
-
-def connectjsonsql(name="unname"):
+def connectjsondsql(namen="unname"):
     #if json file exists:
-    if os.path.exists(name+'.json'):
+    if os.path.exists(namen+'.json') and os.path.getsize(namen+'.json'): 
         print("exist,now connectting")
+    elif os.path.exists(namen+'.json') and not os.path.getsize(namen+'.json'):
+        print("exist,but null")
+        if version == 2:
+            with codecs.open(namen+'.json','w+',encoding='utf-8')as fp:
+                json.dump({},fp,ensure_ascii=False)
+        else:
+            with open(namen+'.json','w+',encoding='utf8')as fp:
+                json.dump({},fp,ensure_ascii=False)
     else:
         print("not exist,now creating and connectting")
         if version == 2:
-            with codecs.open(name+'.json','a',encoding='utf-8')as fp:
-                json.dump("",fp,ensure_ascii=False)
+            with codecs.open(namen+'.json','a+',encoding='utf-8')as fp:
+                json.dump({},fp,ensure_ascii=False)
         else:
-            with open(name+'.json','a',encoding='utf8')as fp:
-                json.dump("",fp,ensure_ascii=False)
-    return name+'.json'
+            with open(namen+'.json','a+',encoding='utf8')as fp:
+                json.dump({},fp,ensure_ascii=False)
+    return namen+'.json'
     
-def cjs(name="unname"):
-    return connectjsonsql(name)
     
-def printj(name):
+def cjds(namen="unname"):
+    return connectjsondsql(namen)
+    
+def printjd(name):
     if version == 2:
-        with codecs.open(name,'r',encoding='utf-8-sig')as fp:
-            if(not bool(fp.read())):
-                print("empty------------------------------------------------")
-                return ""
+        with codecs.open(name,'r+',encoding='utf-8')as fp:
             json_data = json.load(fp)
     else:
-        with open(name,'r',encoding='utf8')as fp:
+        with open(name,'r+',encoding='utf8')as fp:
             json_data = json.load(fp)
-    print('printf data：',json_data)
-    print('printf data type：', type(json_data))
-    if isinstance(json_data, str):
+    print('printf data: ',json_data)
+    print('printf data type: ', type(json_data))
+    if not isinstance(json_data,dict):
         return {}
     else:
         return json_data
 
-def returnj(name):
+def returnjd(name):
     if version == 2:
-        with codecs.open(name,'r',encoding='utf8')as fp:
-            if(not bool(fp.read())):
-                print("empty------------------------------------------------")
-                return ""
+        with codecs.open(name,'r+',encoding='utf-8')as fp:
             json_data = json.load(fp)
     else:
-        with open(name,'r',encoding='utf8')as fp:
+        with open(name,'r+',encoding='utf8')as fp:
             json_data = json.load(fp)
-    if isinstance(json_data, str):
+    if not isinstance(json_data, dict):
         return {}
     else:
         return json_data
-
-
-def renewj(name,dic):
+        
+def renewjd(name,dic):
     if version == 2:
-        with open(name,"w") as f:
-            json.dump(dic,f,ensure_ascii=False)
+        with codecs.open(name,'w+',encoding='utf-8')as fp:
+            json.dump(dic,fp,ensure_ascii=False)
     else:
-        with open(name,"w",encoding='utf8') as f:
-            json.dump(dic,f,ensure_ascii=False)
-    print('printf data：',json_data)
-    print('printf data type：', type(json_data))
-    return json_data
+        with open(name,'w+',encoding='utf8')as fp:
+            json.dump(dic,fp,ensure_ascii=False)
+    return True
+
+#dictionary function 
+#key to value https://www.runoob.com/python/python-dictionary.html
+#value to key https://blog.csdn.net/ywx1832990/article/details/79145576
+
+#list function
+#list https://www.runoob.com/python/python-lists.html
     
-server = cjs(name = "1")
-x = printj(server)
+local = cjds(namen = "1")
+x = printjd(local)
+x['d'] = 123
+
+print( returnjd(local))
+renewjd( local,x )
+print( returnjd(local))
